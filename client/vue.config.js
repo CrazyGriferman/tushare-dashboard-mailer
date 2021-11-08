d2fe51535e94cb9ce36dd8af204e9f3263cf2b46
Crazygriferman <251031557@qq.com>
2021-11-08 17:21:10 +0800

    fix: cors problem

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   // devtool: 'none',
   devServer: {
-    proxy: "http://api.waditu.com",
+    proxy: {
+      "/api": {
+        target: "http://localhost:8085", //the third-party interface you requested
+        changeOrigin: true, // A virtual server is created locally, then the requested data is sent, and the requested data is received at the same time, so there are no cross-domain issues with data interaction between the server and the server
+        pathRewrite: {
+          // Path rewrite,
+          "^/api": "", // Replace the request address in the target, that is, you can write directly to / API when you request the address http://api.douban.com/v2/XXXXX in the future.
+        },
+      },
+      "/": {
+        target: "http://api.waditu.com",
+        changeOrigin: true,
+      },
+    },
   },
 };
