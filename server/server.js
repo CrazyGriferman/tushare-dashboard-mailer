f40bd1251f7e43058bf2be8fba1a5838ab09f0a8
Crazygriferman <251031557@qq.com>
2021-12-20 19:18:11 +0800

    add: backend periodically sendMail demo complete

--- server/server.js
+++ server/server.js
@@ -3,9 +3,24 @@ const nodemailer = require("nodemailer");
 let express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
+const Axios = require("axios");
+
+/* new Date()  to yyyymmdd */
+const dateFormat = (time) => {
+  var date = new Date(time);
+  var year = date.getFullYear();
+  var month =
+    date.getMonth() + 1 < 10
+      ? "0" + (date.getMonth() + 1)
+      : date.getMonth() + 1;
+  var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  // 拼接
+  return year + "" + month + "" + day;
+};
 
 // file module
 const fs = require("fs");
+const { default: axios } = require("axios");
 
 function jsonReader(filePath, cb) {
   fs.readFile(filePath, (err, fileData) => {
@@ -33,20 +48,16 @@ let transporter = nodemailer.createTransport({
   service: "QQ",
   auth: {
     user: "251031557@qq.com", // TODO: your gmail account
-    pass: "ycyucbxqdxnwcbah", // TODO: your gmail password
+    pass: "domvadrtufwibhae", // TODO: your gmail password
+    // domvadrtufwibhae opcatfmlkmhebhca
   },
 });
 
 // setup e-mail data with unicode symbols
 
-app.listen(8085, function () {
-  console.info("复制打开浏览器", "localhost:8085");
-});
-
 app.post("/post", (req, res) => {
   // const data = req.body.data;
   const data = req.body.data;
-  console.log(data);
   let mailOptions = {
     from: "251031557@qq.com", // TODO: email sender
     to: data.mail, // TODO: email receiver
@@ -62,6 +73,8 @@ app.post("/post", (req, res) => {
     return console.log("Email sent!!!");
   });
 });
+
+/* post */
 
 app.get("/subscription", (req, res) => {
   jsonReader("./stock.json", (err, customer) => {
@@ -111,14 +124,6 @@ app.post("/delete", (req, res) => {
   });
 });
 
-app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 const timeFunc = setInterval(() => {
   let date = new Date();
   let h = date.getHours();
@@ -127,37 +132,51 @@ const timeFunc = setInterval(() => {
   }
 }, 60000);
 
-/* // Step 1
-let transporter = nodemailer.createTransport({
-        service: "QQ",
-        auth: {
-          user: "251031557@qq.com", // TODO: your gmail account
-          pass: "domvadrtufwibhae", // TODO: your gmail password
-        },
-      });
-
-// Step 2
-
-let mailOptions = {
-        from: "251031557@qq.com", // TODO: email sender
-        to: "251031557@qq.com", // TODO: email receiver
-      };
-
-// Step 3
-transporter.sendMail(mailOptions, (err) => {
-        if (err) {
-          return console.log("Error occurs");
-        }
-        return console.log("Email sent!!!");
-      });
-
-const mail = {
-  sendMail: function (ob = {}) {
-    console.log(1);
-    transporter.sendMail({ ...mailOptions, ...ob }, (err) => {
-      err && console.log(err);
-    });
-  },
+const fetchDailyStockInfo = (ts_code, trade_data) => {
+  return Axios.default.post("http://api.waditu.com/", {
+    api_name: "daily",
+    token: "f65ba28a7e38e1aa626a720bfa27a7ce1a2d8b14216ad2fc1f44946d",
+    params: {
+      ts_code: ts_code,
+      trade_date: trade_data,
+    },
+    fields: "close",
+  });
 };
 
-module.exports = mail; */
+const sendMail = () => {
+  jsonReader("./stock.json", (err, subscriptionInfo) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const hours = new Date().getHours();
+    if (hours != 18) {
+      const date = dateFormat(new Date());
+      subscriptionInfo.forEach((item) => {
+        fetchDailyStockInfo(item.stockNumber, date).then((res) => {
+          let currentPrice = res.data.data.items;
+          let mailOptions = {
+            from: "251031557@qq.com", // TODO: email sender
+            to: item.mail, // TODO: email receiver
+            subject: `${item.stockName} 股价提醒`,
+            text: `当前${item.stockName}为${currentPrice} , 已小于您订阅的价格${item.stockPrice}`,
+          };
+          transporter.sendMail(mailOptions, (err) => {
+            if (err) {
+              return console.log(err);
+            }
+            res.send("mail send");
+            return console.log("Email sent!!!");
+          });
+        });
+      });
+    }
+  });
+};
+
+/* 3600000 1hour */
+app.listen(8085, function () {
+  console.info("复制打开浏览器", "localhost:8085");
+  setInterval();
+});
